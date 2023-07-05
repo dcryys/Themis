54d9c0e349cbb39d6e5d978a28439af7589664f2
dcryys <138147079+dcryys@users.noreply.github.com>
2023-07-05 19:06:18 +0300

    Added /tellme function
    It also didn't have /datetime function, which is strange, so i copied the code and made it again.

--- themis.py
+++ themis.py
@@ -8,6 +8,7 @@ Serving justice to the world
 import os
 import sys
 from datetime import datetime
+import ai1
 #  pip install python-telegram-bot --upgrade
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
@@ -44,7 +45,26 @@ def time_command(update: Update, context: CallbackContext) -> None:
     currnet_time = the_datetime.time()
     update.message.reply_text(f'Current time is {currnet_time.strftime("%H:%M:%S")}')
 
+"""
+Command /datetime
+"""
+def datetime_command(update: Update, context: CallbackContext) -> None:
+    print(f'User {update.effective_user.first_name} called /datetime')
+    today = datetime.now()
+    current_time = today.time()
+    update.message.reply_text(f'Today is {today.strftime("%A")}, the {today.strftime("%d")} of {today.strftime("%B")}, {today.strftime("%Y")}, {current_time.strftime("%H:%M:%S")}')
 
+
+"""
+Make it speak
+"""
+def tellme_command(update: Update, context: CallbackContext) -> None:
+    print(f'User {update.effective_user.first_name} called /tellme')
+    text = update.effective_message.text
+    answer = ai1.ask_gpt3(text)
+    print(f'User sent us: {update.effective_message.text}')
+    update.message.reply_text(answer)
+    
 """
 Main body of Telegram bot handling loop
 """
@@ -61,6 +81,11 @@ def telegram_func():
     print("Command /hello handler added")
     dispatcher.add_handler(CommandHandler("time", time_command))
     print("Command /time handler added")
+    print("Command /time handler added")
+    dispatcher.add_handler(CommandHandler("datetime", datetime_command))
+    print("Command /datetime handler added")
+    dispatcher.add_handler(CommandHandler("tellme", tellme_command))
+    print("Command /tellme handler added")
 
     updater.start_polling()
     print("Polling started, going into loop")
